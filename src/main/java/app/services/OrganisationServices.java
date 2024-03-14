5b10cda0adea9a0e732d78a8aea50ba8eefd8830
frank-shema <shemafranko0987@gmail.com>
2024-03-14 14:56:05 +0200

    fixes

--- src/main/java/app/services/OrganisationServices.java
+++ src/main/java/app/services/OrganisationServices.java
@@ -2,36 +2,44 @@ package app.services;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
 import app.models.Organisation;
 import app.utils.HibernateUtil;
 
 public class OrganisationServices {
-	protected static SessionFactory sf=HibernateUtil.getSessionFactory();
-	protected static Session session;
-	public static OrganisationServices OrganisationServices;
-	public static OrganisationServices getInstance() {
+    protected static SessionFactory sf = HibernateUtil.getSessionFactory();
+    protected static Session session;
 
-		if (OrganisationServices == null)
-			return new OrganisationServices();
+    public static OrganisationServices OrganisationServices;
 
-		return null;
-	}
-	private OrganisationServices() {
-	}
-	public void addorganisation(Organisation organisation) {
-	    session = sf.openSession();
-		//session=HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
-		session.save(organisation);
-		session.getTransaction().commit();
+    // Singleton pattern to get an instance of OrganisationServices
+    public static OrganisationServices getInstance() {
+        if (OrganisationServices == null) {
+            return new OrganisationServices();
+        }
+        return null;
+    }
 
-	}
-	public Organisation getOrganisationById(int OrganisationId) {
+    // Private constructor to restrict instantiation from outside the class
+    private OrganisationServices() {
+    }
+
+    // Method to add a new organisation to the database
+    public void addorganisation(Organisation organisation) {
         session = sf.openSession();
+        // Begin a transaction
+        session.beginTransaction();
+        // Save the organisation object to the database
+        session.save(organisation);
+        // Commit the transaction
+        session.getTransaction().commit();
+    }
+
+    // Method to retrieve an organisation by its ID
+    public Organisation getOrganisationById(int OrganisationId) {
+        session = sf.openSession();
+        // Retrieve the organisation object by ID
         Organisation organisation = session.get(Organisation.class, OrganisationId);
         session.close(); // Close the session after use
         return organisation;
     }
-
 }
