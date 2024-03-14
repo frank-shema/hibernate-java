00d829c27ac7d150056c4be94609291fb5938314
frank-shema <shemafranko0987@gmail.com>
2024-03-14 14:55:07 +0200

    fixes

--- src/main/java/app/services/PropertyServices.java
+++ src/main/java/app/services/PropertyServices.java
@@ -2,37 +2,44 @@ package app.services;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
 import app.models.Property;
 import app.utils.HibernateUtil;
 
-
 public class PropertyServices {
-	protected static SessionFactory sf=HibernateUtil.getSessionFactory();
-	protected static Session session;
-	public static PropertyServices PropertyServices;
-	public static PropertyServices getInstance() {
+    protected static SessionFactory sf = HibernateUtil.getSessionFactory();
+    protected static Session session;
 
-		if (PropertyServices == null)
-			return new PropertyServices();
+    public static PropertyServices PropertyServices;
 
-		return null;
-	}
-	private PropertyServices() {
-	}
-	public void addProperty(Property agent) {
-	    session = sf.openSession();
-		//session=HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
-		session.save(agent);
-		session.getTransaction().commit();
+    // Singleton pattern to get an instance of PropertyServices
+    public static PropertyServices getInstance() {
+        if (PropertyServices == null) {
+            return new PropertyServices();
+        }
+        return null;
+    }
 
-	}
-	public  Property getPropertyById(int id) {
-		session=sf.openSession();
-		Property property=session.get(Property.class, id);
-		session.close();
-		return property;
-	}
+    // Private constructor to restrict instantiation from outside the class
+    private PropertyServices() {
+    }
 
+    // Method to add a new property to the database
+    public void addProperty(Property property) {
+        session = sf.openSession();
+        // Begin a transaction
+        session.beginTransaction();
+        // Save the property object to the database
+        session.save(property);
+        // Commit the transaction
+        session.getTransaction().commit();
+    }
+
+    // Method to retrieve a property by its ID
+    public Property getPropertyById(int id) {
+        session = sf.openSession();
+        // Retrieve the property object by ID
+        Property property = session.get(Property.class, id);
+        session.close(); // Close the session to release resources
+        return property;
+    }
 }
