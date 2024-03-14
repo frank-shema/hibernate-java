10b5da3085f678f7840519d6ce548a6dc090939f
frank-shema <shemafranko0987@gmail.com>
2024-03-14 14:49:37 +0200

    fixes

--- src/main/java/app/utils/Program.java
+++ src/main/java/app/utils/Program.java
@@ -1,4 +1,5 @@
 package app.utils;
+
 import app.services.UserServices;
 import app.services.PropertyServices;
 import app.services.AgentServices;
@@ -11,26 +12,29 @@ import app.models.User;
 import app.services.AccountServices;
 
 public class Program {
-public static void main(String args[]) {
-	PropertyServices propertyService=PropertyServices.getInstance();
-	UserServices userService=UserServices.getInstance();
-	AgentServices agentService=AgentServices.getInstance();
-	OrganisationServices organisationService=OrganisationServices.getInstance();
-	AccountServices accountServices=AccountServices.getInstance();
-	Organisation org=new Organisation("Estate", "estate@estate.com", "Kigali,Rwanda", "E state is an organisation which have the apartment for rent");
-	organisationService.addorganisation(org);
-	Organisation org1=organisationService.getOrganisationById(1);
-	Agents agent=new Agents("shema", "frank", "+2507535848", "Nyabihu");
-	agentService.addagent(agent);
-	Agents agent1=agentService.getAgentById(1);
-	User user=new User("shema", "frank@gmail.com","Nyabihu", "+250787474");
-	userService.adduser(user);
-	User user1=userService.getUserById(1);
-	Property property=new Property("Kigali Apartment","Kigali Apartment is apartemnt located in Gasabo district it is know for its good service", "Gasabo", 300,agent1,user1);
-	propertyService.addProperty(property);
-	Account account1=new Account("Organisation", "446383769383", 70000, user1);
-	accountServices.addaccount(account1);
-	
+    public static void main(String args[]) {
+        PropertyServices propertyService = PropertyServices.getInstance();
+        UserServices userService = UserServices.getInstance();
+        AgentServices agentService = AgentServices.getInstance();
+        OrganisationServices organisationService = OrganisationServices.getInstance();
+        AccountServices accountServices = AccountServices.getInstance();
 
-}
+        Organisation org = new Organisation("Estate", "estate@estate.com", "Kigali, Rwanda", "E state is an organisation which has the apartment for rent");
+        organisationService.addorganisation(org);
+        Organisation org1 = organisationService.getOrganisationById(1);
+
+        Agents agent = new Agents("shema", "frank", "+2507535848", "Nyabihu");
+        agentService.addagent(agent);
+        Agents agent1 = agentService.getAgentById(1);
+
+        User user = new User("shema", "frank@gmail.com", "Nyabihu", "+250787474");
+        userService.adduser(user);
+        User user1 = userService.getUserById(1);
+
+        Property property = new Property("Kigali Apartment", "Kigali Apartment is an apartment located in Gasabo district known for its good service", "Gasabo", 300, agent1, user1);
+        propertyService.addProperty(property);
+
+        Account account1 = new Account("Organisation", "446383769383", 70000, user1);
+        accountServices.addaccount(account1);
+    }
 }
